a3da64f665c796e43cf138101ad0a0f852a6e8f6
dwikyryan <dwikyryan@gmail.com>
2025-05-18 11:09:50 +0700

    app cutover

--- 01-app-cutover/demo/client/script.js
+++ 01-app-cutover/demo/client/script.js
@@ -1,31 +1,27 @@
-const resultDiv = document.getElementById('result');
+const resultDiv = document.getElementById("result");
 let counter = 0;
 
-// Replace this URL with your actual API endpoint
-const apiUrl = 'https://api.example.com/endpoint';
+const apiUrl = "http://localhost/";
 
 function fetchApiData() {
-    
-    // fetch(apiUrl)
-    //     .then(response => {
-    //         if (!response.ok) {
-    //             throw new Error('Network response was not ok');
-    //         }
-    //         return response.text(); // Assuming the API returns a plain text response
-    //     })
-    //     .then(data => {
-    //         resultDiv.textContent = data; // Display the returned string
-    //     })
-    //     .catch(error => {
-    //         console.error('There was a problem with the fetch operation:', error);
-    //         resultDiv.textContent = 'Error fetching data';
-    //     });
+  fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.text(); // Assuming the API returns a plain text response
+    })
+    .then((data) => {
+      resultDiv.textContent = data + " " + counter++; // Display the returned string
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+      resultDiv.textContent = "Error fetching data";
+    });
 
-    resultDiv.textContent = "Hello World " + counter++;
+  //   resultDiv.textContent = "Hello World " + counter++;
 }
 
-// Call the API every 2 seconds
 setInterval(fetchApiData, 2000);
 
-// Initial call to display data immediately
 fetchApiData();
